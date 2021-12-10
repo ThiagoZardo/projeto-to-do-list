b6fa088c242ee5afa9566964f219cf25044b57fe
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2021-12-10 16:57:04 -0300

    Requisito 07

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+//Adcionar a lista
 let btnAdd = document.getElementById('criar-tarefa');
 let textoDigitado = document.getElementById('texto-tarefa');
 function adcionarNaLista(){
-
     let linha = document.createElement('li');
     if(textoDigitado.value !== ''){
         document.querySelector('ol').appendChild(linha);
         linha.innerText = textoDigitado.value;
         textoDigitado.value = '';
-    }    
+        linha.addEventListener('click', selecionaItem);
+    }      
 }
-
 btnAdd.addEventListener('click', adcionarNaLista);
+
+
+//Selecionar itens na lista
+   
+function selecionaItem(event){
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+}    
+
+
