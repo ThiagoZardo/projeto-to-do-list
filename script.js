718390a8269b7f69c8bd5bd370b247e0129a4f40
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2021-12-11 14:14:51 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -2,31 +2,42 @@
 let textoDigitado = document.getElementById('texto-tarefa');
 let btnAdd = document.getElementById('criar-tarefa');
 
-
 function adcionarNaLista(){
     var linha = document.createElement('li');
     if(textoDigitado.value !== ''){
         document.querySelector('ol').appendChild(linha);
         linha.innerText = textoDigitado.value;
         textoDigitado.value = '';
-        linha.addEventListener('click', selecionaItem);        
+        linha.addEventListener('click', selecionaItem);
+        linha.addEventListener('dblclick', riscar);     
     };
-    linha.classList.add('list-item');      
+    linha.classList.add('itemLista');      
 };
 btnAdd.addEventListener('click', adcionarNaLista);
 
+
 //Selecionar itens na lista
-//
 function selecionaItem(event){
     limparCor();
     event.target.style.backgroundColor = 'rgb(128,128,128)';;
 }
 
-
 function limparCor(){
-    let limpa = document.querySelectorAll('.list-item');
+    let limpa = document.querySelectorAll('.itemLista');
     for(let i = 0; i < limpa.length; i+=1){
         limpa[i].style.backgroundColor = 'white';
     }
-    
 }
+
+//Tarefas Realizadas
+function riscar(event){
+    if(event.target.classList.contains('completed')){
+        event.target.classList.remove('completed');
+    }else{
+        event.target.classList.add('completed');
+    }
+
+    //event.target.classList.toggle('completed');
+}
+
+//Limpa tudo
