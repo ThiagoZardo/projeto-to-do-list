3548398bb00cc91e01007d6092cc437dac06db9c
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2021-12-11 00:00:56 -0300

    Requisito 08

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 //Adcionar a lista
-let btnAdd = document.getElementById('criar-tarefa');
 let textoDigitado = document.getElementById('texto-tarefa');
+let btnAdd = document.getElementById('criar-tarefa');
+
+
 function adcionarNaLista(){
-    let linha = document.createElement('li');
+    var linha = document.createElement('li');
     if(textoDigitado.value !== ''){
         document.querySelector('ol').appendChild(linha);
         linha.innerText = textoDigitado.value;
         textoDigitado.value = '';
-        linha.addEventListener('click', selecionaItem);
-    }      
-}
+        linha.addEventListener('click', selecionaItem);        
+    };
+    linha.classList.add('list-item');      
+};
 btnAdd.addEventListener('click', adcionarNaLista);
 
-
 //Selecionar itens na lista
-   
+//
 function selecionaItem(event){
-    event.target.style.backgroundColor = 'rgb(128,128,128)';
-}    
+    limparCor();
+    event.target.style.backgroundColor = 'rgb(128,128,128)';;
+}
 
 
+function limparCor(){
+    let limpa = document.querySelectorAll('.list-item');
+    for(let i = 0; i < limpa.length; i+=1){
+        limpa[i].style.backgroundColor = 'white';
+    }
+    
+}
